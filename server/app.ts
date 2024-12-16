94dd812113def11c3fdebce4e1bff69c0a2bd4b7
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-12-16 13:07:45 +0530

    injected a middleware

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,6 @@
 import { createRequestHandler } from "@react-router/express";
 import express from "express";
+import type { Request, Response, NextFunction } from "express";
 import "react-router";
 
 declare module "react-router" {
@@ -9,6 +10,34 @@ declare module "react-router" {
 }
 
 const app = express();
+
+function getCookies(req: Request) {
+  const headerCookies = req.headers.cookie;
+
+  const cookies = headerCookies ? headerCookies.split("; ") : [];
+
+  const cookiesJson: Record<string, string> = {};
+
+  cookies.forEach((cookie) => {
+    const [key, value] = cookie.split("=");
+    cookiesJson[key] = value;
+  });
+
+  return cookiesJson;
+}
+
+const middleware = (req: Request, res: Response, next: NextFunction) => {
+  console.log("caught you");
+
+  console.log("the incoming request is at", req.originalUrl);
+
+  const cookies = getCookies(req);
+
+  // console.log("the cookies are:", cookies);
+  next();
+};
+
+app.use(middleware);
 
 app.use(
   createRequestHandler({
